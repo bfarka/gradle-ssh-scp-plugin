2454267816e3b68fc3601eca6dc90b8aaee70321
berndfarka <bernd.farka@gmail.com>
2014-11-15 09:11:11 +0100

    add empty plugin definition

---
+++ src/main/groovy/at/berndfarka/gradle/sshscp/SshScpPlugin.groovy
@@ -0,0 +1,14 @@
+package at.berndfarka.gradle.sshscp
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by berndfarka on 15.11.14.
+ */
+class SshScpPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
