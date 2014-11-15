d01a882484c6e9e11da100f233efd54b2365fa4b
berndfarka <bernd.farka@gmail.com>
2014-11-15 09:54:44 +0100

    add dependencies for unittesting

--- src/test/groovy/at/berndfarka/gradle/sshscp/SshScpPluginTest.groovy
+++ src/test/groovy/at/berndfarka/gradle/sshscp/SshScpPluginTest.groovy
@@ -1,7 +1,33 @@
 package at.berndfarka.gradle.sshscp
 
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.junit.Assert
+import org.junit.Test
+import java.util.Properties
+
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertNotNull
+
 /**
  * Created by berndfarka on 15.11.14.
  */
-class SshScpPluginTest {
+public class SshScpPluginTest {
+
+    @Test
+    public void testPluginRegistration(){
+        InputStream propertiesFileStream = ClassLoader.systemClassLoader.getResourceAsStream("META-INF/gradle-plugins/at.berndfarka.sshscp.properties")
+        assertNotNull(propertiesFileStream);
+
+        Properties prop = new Properties()
+        prop.load(propertiesFileStream);
+        String implClass = prop.getProperty("implementation-class");
+
+        Class<Plugin> pluginClass = Class.forName(implClass);
+        Plugin<Project> sshScpPlugin = pluginClass.newInstance();
+        assertNotNull(sshScpPlugin);
+        Assert.assertTrue(sshScpPlugin instanceof SshScpPlugin);
+
+    }
+
 }
